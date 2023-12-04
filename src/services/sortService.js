93074f3c0190556d25a5ef1756ec6a9253fe9aa6
BlindEngineer <Korovamumu2005@list.ru>
2023-12-05 00:53:07 +0300

    - changed toSorted method to Sort method

--- src/services/sortService.js
+++ src/services/sortService.js
@@ -1,12 +1,28 @@
 export default function sortTickets(tickets, tab) {
   if (tab === 'cheapest') {
-    return tickets.toSorted((prev, next) => {
+    return tickets.sort((prev, next) => {
       return prev.price - next.price
     })
   }
-  return tickets.toSorted((prev, next) => {
+  return tickets.sort((prev, next) => {
     const prevTotalDuration = prev.segments[0].duration + prev.segments[1].duration
     const nextTotalDuration = next.segments[0].duration + next.segments[1].duration
     return prevTotalDuration - nextTotalDuration
   })
 }
+
+// Вариант кода содержит метод toSorted, поддержка которого составляет 86%
+// Не работает на некоторых версиях Safari, Chrome for Android и многих других.
+//
+// export default function sortTickets(tickets, tab) {
+//   if (tab === 'cheapest') {
+//     return tickets.toSorted((prev, next) => {
+//       return prev.price - next.price
+//     })
+//   }
+//   return tickets.toSorted((prev, next) => {
+//     const prevTotalDuration = prev.segments[0].duration + prev.segments[1].duration
+//     const nextTotalDuration = next.segments[0].duration + next.segments[1].duration
+//     return prevTotalDuration - nextTotalDuration
+//   })
+// }
